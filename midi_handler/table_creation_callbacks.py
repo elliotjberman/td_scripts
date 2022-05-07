f94190a02e04b4deb2397dd7fe418dc80df40dc5
Elliot Berman <elliotjberman@gmail.com>
2022-05-07 13:24:44 -0400

    cleanup

--- midi_handler/table_creation_callbacks.py
+++ midi_handler/table_creation_callbacks.py
@@ -1,27 +1,7 @@
+SPACING = 200
+
 def onFindOPGetInclude(dat, curOp, row):
 	return True
-
-
-# Provide an extensive dictionary of what was matched for each operator.
-# Multiple matching tags, parameters and cells will be included.
-# For each match, a corresponding key is included in the dictionary:
-#
-#  results:
-#
-#  'name': curOp.name
-#  'type': curOp.OPType
-#  'path': curOp.path
-#  'parent' : curOp.parent()
-#  'comment': curOp.comment
-#  'tags' : [list of strings] or empty list
-#  'text' : [list of Cells] or empty list
-#  'par': dictionary of matching parameter attributes.
-#    example entries:
-#        tx : { 'name': True, 'value':True , 'expression':True } # Parameter tx matched on name, value, expression
-#        ty : { 'value' : True } # Parameter ty matched on value
-#
-
-SPACING = 200
 
 def onOPFound(dat, curOp, row, results):
 	midi_table = create_or_get_midi_table(curOp)
