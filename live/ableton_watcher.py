aa4f33c1fcf302d6a2ca197b219e445c1b01a611
Elliot Berman <elliotjberman@gmail.com>
2022-05-14 18:40:20 -0400

    crossfade the visuals

---
+++ live/ableton_watcher.py
@@ -0,0 +1,52 @@
+# me - this DAT
+#
+# channel - the Channel object which has changed
+# sampleIndex - the index of the changed sample
+# val - the numeric value of the changed sample
+# prev - the previous sample value
+#
+# Make sure the corresponding toggle is enabled in the CHOP Execute DAT.
+
+def onOffToOn(channel, sampleIndex, val, prev):
+    return
+
+def whileOn(channel, sampleIndex, val, prev):
+    return
+
+def onOnToOff(channel, sampleIndex, val, prev):
+    return
+
+def whileOff(channel, sampleIndex, val, prev):
+    return
+
+def onValueChange(channel, sampleIndex, val, prev):
+    on = bool(channel)
+    if on:
+        track_name = get_name_of_track()
+        turn_on_visual(track_name)
+    else:
+        turn_on_placeholder()
+    return
+
+def turn_on_visual(name: str) -> None:
+    ableton_switcher = op("ableton_switcher")
+    visual_name = f"{name}_visual"
+    for index, connector in enumerate(ableton_switcher.inputs):
+        operator = connector.parent()
+        if operator is op(visual_name):
+            operator.allowCooking = True
+            ableton_switcher.par.index = index
+        else:
+            operator.allowCooking = False
+
+    toggle_placeholder(True)
+
+def turn_on_placeholder() -> None:
+    op('placeholder').allowCooking = True
+    toggle_placeholder(False)
+
+def get_name_of_track() -> str:
+    return "headphones_kid" if op('test_toggle')[0] else "pipes"
+
+def toggle_placeholder(is_on: bool) -> None:
+    parent().par.Index = int(is_on)
