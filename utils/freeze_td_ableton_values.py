9b97fde0d4d23fbec3de02f95fe82705b3da02ad
Elliot Berman <elliotjberman@gmail.com>
2025-03-26 18:40:27 -0400

    update TD freezing script to include MIDI components, a little cleaner overall also

--- utils/freeze_td_ableton_values.py
+++ utils/freeze_td_ableton_values.py
@@ -4,11 +4,21 @@
 # This goes through and hardcodes the indices of all the tracks/chains so even if you open the wrong Ableton set, the TDAbleton COMPs
 # should not get altered.
 
+# Use with an OPFind input looking for "Other COMPs".
+
 DONT_ALTER = (
 	"Autosync",
 	"Valuesend",
 	"Mapfromlive",
 )
+
+MIDI_PARAMS = (
+	"Track",
+	"Device",
+)
+
+ABLETON_PARAMETER = "Ableton Parameter"
+ABLETON_MIDI = "Ableton MIDI"
 
 def onTableChange(dat):
 	for row in dat.rows():
@@ -16,16 +26,37 @@ def onTableChange(dat):
 		if comp is None:
 			continue
 	
-		for parameter in comp.customPages["Ableton Parameter"].pars:
-			if parameter.hidden or parameter.val is None or parameter.name in DONT_ALTER:
-				continue
+		# Because TD doesn't have COMP type info
+		if ABLETON_PARAMETER in comp.customPages:
+			handle_ableton_parameter(comp)
+			continue
 
-			value = parameter.menuIndex
-			if value is None:
-				continue
+		if ABLETON_MIDI in comp.customPages:
+			handle_ableton_midi(comp)
 
-			parameter.expr = f"{value} + 0"
-			parameter.mode = ParMode.EXPRESSION
+
+def handle_ableton_parameter(comp):
+	for parameter in comp.customPages[ABLETON_PARAMETER].pars:
+		if parameter.hidden or parameter.val is None or parameter.name in DONT_ALTER:
+			continue
+
+		freeze_parameter(parameter)
+
+def handle_ableton_midi(comp):
+	for parameter in comp.customPages["Ableton MIDI"].pars:
+		if parameter.hidden or parameter.val is None or parameter.name not in MIDI_PARAMS:
+			continue
+
+		freeze_parameter(parameter)
+
+
+def freeze_parameter(parameter):
+	value = parameter.menuIndex
+	if value is None:
+		return
+
+	parameter.expr = f"{value} + 0"
+	parameter.mode = ParMode.EXPRESSION
 
 def onRowChange(dat, rows):
 	return
